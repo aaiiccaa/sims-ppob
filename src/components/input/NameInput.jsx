73ce3a805b088aaab4dbb0c69961aa305b825213
aaiiccaa <aishafarizka.a@gmail.com>
2025-10-21 23:28:08 +0700

    update profile api

--- src/components/input/NameInput.jsx
+++ src/components/input/NameInput.jsx
@@ -1,6 +1,6 @@
 import { User } from "lucide-react";
 
-const NameInput = ({ placeholder = "Nama", value, onChange, className }) => {
+const NameInput = ({ placeholder = "Nama", value, onChange, className, disabled = false }) => {
     return (
         <div className="relative w-full">
             <User className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
@@ -9,6 +9,7 @@ const NameInput = ({ placeholder = "Nama", value, onChange, className }) => {
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
+                disabled={disabled}
                 className={`border border-gray-300 text-sm rounded-md px-4 py-2 w-full pl-10 focus:outline-none focus:ring-1 focus:ring-primary ${className || ""}`}
             />
         </div>
--- src/pages/Akun.jsx
+++ src/pages/Akun.jsx
@@ -4,6 +4,9 @@ import NameInput from "../components/input/NameInput"
 import Button from "../components/Button"
 import { useDispatch } from "react-redux"
 import { removeToken } from "../store/authSlice"
+import { useEffect, useState } from "react"
+import api from "../axios/api"
+import { toast } from "react-toastify"
 
 const Akun = () => {
     const dispatch = useDispatch();
@@ -12,40 +15,118 @@ const Akun = () => {
         dispatch(removeToken());
     }
 
+    const [data, setData] = useState()
+    const [loading, setLoading] = useState(false)
+    const [payload, setPayload] = useState({
+        first_name: "",
+        last_name: ""
+    })
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const getProfile = async () => {
+        try {
+            setLoading(true)
+            const res = await api.get("/profile")
+            setData(res.data.data)
+            setPayload({
+                first_name: res.data.data.first_name,
+                last_name: res.data.data.last_name
+            })
+        } catch (err) {
+            toast.error(err.response.data.message)
+        } finally {
+            setLoading(true)
+        }
+    }
+
+    const handleUpdateProfile = async () => {
+        try {
+            const res = await api.put("/profile/update", payload)
+            console.log(res)
+            setIsEditing(false)
+            setData({
+                ...data, last_name: payload.last_name, first_name: payload.first_name
+            })
+            toast.success("Berhasil update profile!")
+        } catch (err) {
+            toast.error(err.response.data.message)
+        }
+    }
+
+    useEffect(() => {
+        getProfile()
+    }, [])
+
     return (
-        <div className="w-full flex justify-center px-4 py-4">
-            <div className="max-w-3xl w-full flex flex-col justify-center gap-6">
+        <>
+            {data ? (
+                <div className="w-full flex justify-center px-4 py-4">
+                    <div className="max-w-3xl w-full flex flex-col justify-center gap-6">
 
-                <div className="flex flex-col items-center justify-center">
-                    <img className="w-24" src={avatar} alt="" />
-                    <div className="mt-2 text-xl font-semibold">Kristanto Wibowo</div>
-                </div>
+                        <div className="flex flex-col items-center justify-center">
+                            <img className="w-24" src={avatar} alt="Avatar" />
+                            <div className="mt-2 text-xl font-semibold">
+                                {data.first_name} {data.last_name}
+                            </div>
+                        </div>
 
-                <div className="text-sm">
-                    <div>
-                        <div className="mb-1 font-medium">Email</div>
-                        <EmailInput />
+                        <div className="text-sm">
+                            <div>
+                                <div className="mb-1 font-medium">Email</div>
+                                <EmailInput
+                                    disabled
+                                    placeholder="Email"
+                                    value={data.email}
+                                />
+                            </div>
+
+                            <div>
+                                <div className="mt-4 mb-1 font-medium">Nama Depan</div>
+                                <NameInput
+                                    disabled={!isEditing}
+                                    placeholder="Nama depan"
+                                    value={payload.first_name}
+                                    onChange={(e) => setPayload({
+                                        ...payload, first_name: e.target.value
+                                    })}
+                                />
+                            </div>
+
+                            <div>
+                                <div className="mt-4 mb-1 font-medium">Nama Belakang</div>
+                                <NameInput
+                                    disabled={!isEditing}
+                                    placeholder="Nama belakang"
+                                    value={payload.last_name}
+                                    onChange={(e) => setPayload({
+                                        ...payload, last_name: e.target.value
+                                    })}
+                                />
+                            </div>
+                        </div>
+                        {
+                            isEditing ? (
+                                    <div className="flex flex-col gap-4">
+                                        <Button children={"Simpan"} onClick={handleUpdateProfile} />
+                                    </div>
+                            ) : (
+                                <div className="flex flex-col gap-4">
+                                    <Button children={"Edit Profil"} onClick={() => { setIsEditing(true) }} />
+                                    <Button children={"Logout"} secondary onClick={handleLogout} />
+                                </div>
+                            )
+                        }
                     </div>
-
-                    <div>
-                        <div className="mt-4 mb-1 font-medium">Nama Depan</div>
-                        <NameInput />
-                    </div>
-
-                    <div>
-                        <div className="mt-4 mb-1 font-medium">Nama Belakang</div>
-                        <NameInput />
-                    </div>
                 </div>
-
-                <div className="flex flex-col gap-4">
-                    <Button children={"Edit Profil"} />
-                    <Button children={"Logout"} secondary onClick={handleLogout}/>
+            ) : (
+                <div className="w-full flex justify-center items-center">
+                    <p>Loading...</p>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
+    );
 
-    )
 }
 
 export default Akun
