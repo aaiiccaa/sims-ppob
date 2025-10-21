81faea35104f7b686743777b82428630c98d7658
aaiiccaa <aishafarizka.a@gmail.com>
2025-10-21 22:27:14 +0700

    fix activestate on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center shadow-sm px-24 py-4">
             <div className="">
-                <Link to="/">
+                <NavLink to="/">
                     <div className='flex gap-2 font-semibold text-lg justify-center items-center'>
                         <img src={logo} alt="Logo" className='w-5 h-5' />
                         SIMS PPOB
                     </div>
-                </Link>
+                </NavLink>
             </div>
-            <ul className="flex gap-10">
-                <li><Link to="/topup">Top up</Link></li>
-                <li><Link to="/transaction">Transaction</Link></li>
-                <li><Link to="/akun">Akun</Link></li>
+            <ul className="flex gap-10 text-sm">
+                <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/topup">Top up</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/transaction">Transaction</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/akun">Akun</NavLink></li>
             </ul>
         </nav>
     );
