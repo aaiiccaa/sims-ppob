64c78ab7550f16525f9a89d4fa21ab8b3ca96a32
aaiiccaa <aishafarizka.a@gmail.com>
2025-10-22 10:38:32 +0700

    fix logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
         <nav className="flex justify-between items-center shadow-sm px-24 py-4">
             <div className="">
                 <NavLink to="/">
-                    <div className='flex gap-2 font-semibold text-lg justify-center items-center'>
-                        <img src={logo} alt="Logo" className='w-5 h-5' />
+                    <div className='flex gap-2 font-semibold text-sm justify-center items-center'>
+                        <img src={logo} alt="Logo" className='w-4 h-4' />
                         SIMS PPOB
                     </div>
                 </NavLink>
             </div>
             <ul className="flex gap-10 text-sm">
                 <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/topup">Top up</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/transaction">Transaction</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/transactions">Transaction</NavLink></li>
                 <li><NavLink className={({ isActive }) => isActive ? 'text-red' : ''} to="/akun">Akun</NavLink></li>
             </ul>
         </nav>
