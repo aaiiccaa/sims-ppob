dbf2deb6fb9d39b010b25af34b39f2bc739589a6
aaiiccaa <aishafarizka.a@gmail.com>
2025-10-22 11:29:10 +0700

    fix input and button component in topup page

---
+++ src/components/input/PriceInput.jsx
@@ -0,0 +1,18 @@
+import { Banknote } from "lucide-react";
+
+const PriceInput = ({ placeholder = "Nominal", value, onChange, className }) => {
+    return (
+        <div className="relative w-full">
+            <Banknote className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                className={`border border-gray-300 text-sm rounded-md px-4 py-2 w-full pl-10 focus:outline-none focus:ring-1 focus:ring-primary ${className || ""}`}
+            />
+        </div>
+    );
+};
+
+export default PriceInput;
--- src/page-components/TopUp.jsx
+++ src/page-components/TopUp.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react"
 import Button from "../components/Button"
-import Input from "../components/input/Input"
-import ProfileBalanceSection from "./ProfileBalanceSection"
 import api from "../axios/api"
 import { toast } from "react-toastify"
+import PriceInput from "../components/input/PriceInput"
 
 const TopUp = () => {
     const [value, setValue] = useState()
@@ -37,10 +36,9 @@ const TopUp = () => {
             <div className="flex gap-4">
                 <div className="w-2/3 flex flex-col gap-4">
                     <div>
-                        <Input 
+                        <PriceInput 
                         value={value}
                         onChange={(e) => setValue(e.target.value)}
-                        placeholder={"Masukkan nominal top up"}
                         />
                     </div>
                     <div>
@@ -48,12 +46,12 @@ const TopUp = () => {
                     </div>
                 </div>
                 <div className="w-1/3 grid grid-cols-3 gap-2">
-                    <Button secondary children={"Rp10.000"} onClick={() => {setValue(10000)}}/>
-                    <Button secondary children={"Rp20.000"} onClick={() => {setValue(20000)}}/>
-                    <Button secondary children={"Rp50.000"} onClick={() => {setValue(50000)}}/>
-                    <Button secondary children={"Rp100.000"} onClick={() => {setValue(100000)}}/>
-                    <Button secondary children={"Rp250.000"} onClick={() => {setValue(250000)}}/>
-                    <Button secondary children={"Rp500.000"} onClick={() => {setValue(500000)}}/>
+                    <Button tertiary children={"Rp10.000"} onClick={() => {setValue(10000)}}/>
+                    <Button tertiary children={"Rp20.000"} onClick={() => {setValue(20000)}}/>
+                    <Button tertiary children={"Rp50.000"} onClick={() => {setValue(50000)}}/>
+                    <Button tertiary children={"Rp100.000"} onClick={() => {setValue(100000)}}/>
+                    <Button tertiary children={"Rp250.000"} onClick={() => {setValue(250000)}}/>
+                    <Button tertiary children={"Rp500.000"} onClick={() => {setValue(500000)}}/>
                 </div>
             </div>
         </div>
