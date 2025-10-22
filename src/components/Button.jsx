af41ceedd2caf40d6e37076e6325fa7987f3b922
aaiiccaa <aishafarizka.a@gmail.com>
2025-10-22 11:30:20 +0700

    change button border color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ type = "button", children, className, onClick, secondary, disa
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`${secondary ? "text-red border border-red hover:bg-red-100": tertiary ? "text-gray-700 border border-gray-700 hover:bg-red-50" : textOnly ? "text-red" : "bg-red text-white  hover:bg-red-600"}  text-sm rounded-md px-auto py-3 w-full ${disabled ? "cursor-not-allowed" : "cursor-pointer"} transition-colors ${className || ""}`}
+            className={`${secondary ? "text-red border border-red hover:bg-red-100": tertiary ? "text-gray-700 border border-gray-300 hover:bg-red-50" : textOnly ? "text-red" : "bg-red text-white  hover:bg-red-600"}  text-sm rounded-md px-auto py-3 w-full ${disabled ? "cursor-not-allowed" : "cursor-pointer"} transition-colors ${className || ""}`}
         >
             {children}
         </button>
